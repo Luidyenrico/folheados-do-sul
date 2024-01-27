25cec2871d8d4f6e80ce240128db04a44c6be817
LuidyEnrico <80763934+Luidyenrico@users.noreply.github.com>
2024-01-27 17:33:10 -0300

    Criando menu

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,22 @@ botaoWhats.addEventListener("click", function() {
 });
 }
 
+function Abre_Fecha_Menu () {
+    let botaoMenu = document.getElementById("botao-menu")
+    let menuOpcoes = document.getElementById("menu")
+
+    botaoMenu.addEventListener("click", function() {
+        if (menuOpcoes.style.display == 'block') {
+            menuOpcoes.style.display = 'none'
+            botaoMenu.src = "../src/imagens/menu.png"
+            botaoMenu.style.width = '27px'
+       } else {
+            menuOpcoes.style.display = 'block'
+            botaoMenu.src = "../src/imagens/fechar.png"
+            botaoMenu.style.width = '20px'
+       }
+    })
+}
+
+Abre_Fecha_Menu()
 Abre_Pagina()
